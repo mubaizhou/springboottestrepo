192bd516b47f8e0cff1d31aa42a3365cfe28e3c3
慕永康 <muyk@upay360.com>
2017-09-19 16:41:47 +0800

    second commit

---
+++ src/test/java/com/lanze/springboot/test/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.lanze.springboot.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * @author muyk@upay360.com
+ * @date 2017年9月19日 下午4:34:33
+ * @description 
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        
+    }
+}
